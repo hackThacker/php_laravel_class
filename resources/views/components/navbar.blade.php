c63e5b338f450fbf42decaf0c02e9184e77667f7
hackThacker <nirajadhiakri572@gmail.com>
2024-07-23 22:10:41 +0545

    added component navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,26 @@
+<div>
+    <header class="bg-blue-700 text-white shadow-md">
+        <div class="container mx-auto px-4 py-5 flex justify-between items-center">
+            <!-- Logo -->
+            <div class="text-2xl font-semibold">
+                <a href="/" class="hover:text-gray-300">Logo</a>
+            </div>
+
+            <!-- Navigation -->
+    <nav>
+        <ul class="flex space-x-6">
+            <li>
+                <a href="/" class="hover:text-gray-300">Home</a>
+            </li>
+            <li>
+                <a href="/contactus" class="hover:text-gray-300">Contact</a>
+            </li>
+            <li>
+                <a href="/about" class="hover:text-gray-300">About</a>
+            </li>
+            <li><a href="/welcome" class="hover:text-gray-300">Welcome</a></li>
+        </ul>
+    </nav>
+</div>
+</header>
+</div>
