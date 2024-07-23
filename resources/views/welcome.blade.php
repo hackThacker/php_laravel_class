803cc5df591ce5593a6a2d069bf9e042a7b2d3e1
hackThacker <nirajadhiakri572@gmail.com>
2024-07-23 22:11:57 +0545

    add tailwind update code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 <body>
+    <x-navbar/>
     <h1>
 Welcome pages
 
--- routes/web.php
+++ routes/web.php
@@ -2,66 +2,62 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Define a GET route for the root URL ('/')
+// When accessed, this route will return the 'welcome' view
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
-
+// Define a GET route for the '/about' URL
+// When accessed, this route will return the 'about' view
 Route::get('/about', function () {
     return view('about');
 });
 
+// Define a GET route for the '/contactus' URL
+// When accessed, this route will return the 'contactus' view
 Route::get('/contactus', function () {
     return view('contactus');
 });
 
-// Define a GET route in Laravel that matches the URL pattern '/niraj/{p}'
-// When a user navigates to this URL, the function provided will be executed
-Route::get('/niraj/{p}', function ($p) {
-
-    // The function takes one parameter: $p
-    // This parameter is extracted from the URL segment
-    // For example, if the URL is '/niraj/123', then $p = 123
-
-    // The function will return the value of $p
-    // In this example, the return value would be 123
-    return $p;
-
-    // Note: This function assumes that the URL segment is a single value
-    // If additional segments are added, they will not be handled by this route
+Route::get('/welcome', function(){
+    return view('welcome');
 });
 
+// Define a GET route for the '/niraj/{p}' URL
+// The {p} parameter is a placeholder for a dynamic value
+Route::get('/niraj/{p}', function ($p) {
+    // The function receives the {p} parameter from the URL
+    // Example: If the URL is '/niraj/123', then $p will be '123'
 
-// Define a GET route in Laravel that matches the URL pattern '/admin/{p}/{a}/{b}'
-// When a user navigates to this URL, the function provided will be executed
+    // Return the value of $p
+    // In the example, the return value would be '123'
+    return $p;
+});
+
+// Define a GET route for the '/admin/{p}/{a}/{b}' URL
+// The {p}, {a}, and {b} parameters are placeholders for dynamic values
 Route::get('/admin/{p}/{a}/{b}', function ($p, $a, $b) {
+    // The function receives the {p}, {a}, and {b} parameters from the URL
+    // Example: If the URL is '/admin/1/2/3', then $p will be '1', $a will be '2', and $b will be '3'
 
-    // The function takes three parameters: $p, $a, and $b
-    // These parameters are extracted from the URL segments
-    // For example, if the URL is '/admin/1/2/3', then $p = 1, $a = 2, and $b = 3
-
-    // The function will return the sum of these three parameters
-    // In this example, the return value would be 6 (1 + 2 + 3)
-    // In this example, the return value return + to add . to join
+    // Concatenate the values of $p, $a, and $b and return the result
+    // In the example, the return value would be '123'
     return $p . $a . $b;
 
-    // Note: This function assumes that the URL segments are numeric
-    // If non-numeric values are passed, the behavior may not be as expected
+    // Note: The values are treated as strings and concatenated
+    // Ensure that the URL segments are appropriate for concatenation
 });
 
-// Define a GET route in Laravel that matches the URL pattern '/contact/{a}/{b}'
-// When a user navigates to this URL, the function provided will be executed
+// Define a GET route for the '/dynamic/{a}/{b}' URL
+// The {a} and {b} parameters are placeholders for dynamic values
 Route::get('/dynamic/{a}/{b}', function($a, $b) {
+    // The function receives the {a} and {b} parameters from the URL
+    // Example: If the URL is '/dynamic/John/Doe', then $a will be 'John' and $b will be 'Doe'
 
-    // The function takes two parameters: $a and $b
-    // These parameters are extracted from the URL segments
-    // For example, if the URL is '/contact/John/Doe', then $a = 'John' and $b = 'Doe'
-
-    // The function will return a view called 'contactus'
-    // The compact function is used to pass the variables $a and $b to the view
-    // This means the 'contactus' view will have access to $a and $b as variables
+    // Return the 'dynamic' view, passing the variables $a and $b to it
+    // The view will have access to $a and $b as variables
     return view('dynamic', compact('a', 'b'));
 
-    // Note: Ensure that the 'contactus' view exists in the resources/views directory
-    // The view will use the values of $a and $b as needed
+    // Note: Ensure that the 'dynamic' view exists in the resources/views directory
 });
