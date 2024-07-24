4a69472661c5b3fa15a89e7c1897842e6dabab8d
hackThacker <nirajadhiakri572@gmail.com>
2024-07-24 22:00:34 +0545

    props added

--- resources/views/components/frontend-layout.blade.php
+++ resources/views/components/frontend-layout.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-
+    @props(['title'])
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Niraj {{$title ?? ''}}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 </head>
 <body>
     <header>
